c7acd025d8f537fef1827fdc078cc3acf8fa8a76
xiaowen1231 <131440008+xiaowen1231@users.noreply.github.com>
2023-07-13 23:07:34 +0800

    回家作業
    回家作業

---
+++ Controllers/TravelController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AjaxHomework.Controllers
+{
+    public class TravelController : Controller
+    {
+        public IActionResult TouristAttractions()
+        {
+            return View();
+        }
+    }
+}
