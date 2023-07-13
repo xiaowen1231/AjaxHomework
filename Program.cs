7f335518724aac6ee3b6c0ec79a456bbbf01e0e1
xiaowen1231 <131440008+xiaowen1231@users.noreply.github.com>
2023-07-13 23:30:26 +0800

    回家作業

--- Program.cs
+++ Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Travel}/{action=TouristAttraction}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
