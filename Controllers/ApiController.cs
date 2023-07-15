a8ed3cd17474b28d047829e9a935f6cc36d87a75
xiaowen1231 <131440008+xiaowen1231@users.noreply.github.com>
2023-07-15 19:19:08 +0800

    作業四

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,43 @@
+﻿using AjaxHomework.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AjaxHomework.Controllers
+{
+    public class ApiController : Controller
+    {
+        private readonly DemoContext _context;
+
+        public ApiController(DemoContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Cities()
+        {
+            var cities = _context.Address.Select(c=>c.City).Distinct();
+            return Json(cities);
+        }
+
+        public IActionResult Districts(string city)
+        {
+            var districts = _context.Address
+                .Where(a=>a.SiteId.Contains(city))
+                .Select(a=>a.SiteId.Substring(3,3))
+                .Distinct();
+            return Json(districts);
+        }
+
+        public IActionResult Road(string district)
+        {
+            var road = _context.Address
+                .Where(a => a.SiteId == district)
+                .Select(a => a.Road);
+                
+            return Json(road);
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace AjaxHomework.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Address()
+        {
+            return View();
+        }
     }
 }
